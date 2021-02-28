913b0ab809219725f146598851a74114c3e207f0
Andrei Toledo <actoledo.andrei@gmail.com>
2021-02-28 15:39:30 -0300

    Inclusão de alguns outros tratamentos de erros.

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -8,8 +8,14 @@ export async function showContas (req: Request, res: Response) {
             .select('*')
             .leftJoin('regras_atraso', 'contas.id_regra', 'regras_atraso.id_regra');
 
+        if (contas.length < 1) return res.status(204).json({msg: 'Nenhuma conta encontrada.', contas});
+
         return res.status(200).json({ msg: 'Contas encontradas com sucesso.', contas });
     } catch(e) {
+        if (e && e.code && e.code === 'ECONNREFUSED'){
+            return res.status(503).json({msg:'Falha na comunicação com o banco de dados. Tente novamente.'});
+        }
+
         return res.status(500).json({msg: 'Erro ao buscar contas.', error: e});
     }
 }
@@ -20,27 +26,28 @@ export async function createConta (req: Request, res: Response) {
         const regras = await knex('regras_atraso').select('*');
         const diferenca_dias = moment(data_pagamento, 'YYYY-MM-DD').diff(moment(data_vencimento, 'YYYY-MM-DD'), 'days');
         let valor_final = valor_original;
-        const regra_applicada = regras.filter( regra => {
+        const regra_aplicada = regras.filter( regra => {
             if(!regra.qtd_dias_final) regra.qtd_dias_final = Infinity;
             return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias;
         });
 
-        if(regra_applicada.length > 0){
+        if(regra_aplicada.length > 0){
             switch (tipo_juros){
                 case 'composto':
-                    valor_final = valor_original * ( ( 1 + (regra_applicada[0].qtd_juros / 100 ) ) ^ diferenca_dias );
-                    valor_final += (valor_original * regra_applicada[0].qtd_multa) / 100;
+                    valor_final = valor_original * ( Math.pow( 1 + ( regra_aplicada[0].qtd_juros / 100 ) , diferenca_dias ) );
+                    // console.log('difdias',diferenca_dias)
+                    valor_final += ( valor_original * regra_aplicada[0].qtd_multa ) / 100;
                     valor_final = valor_final.toFixed(2);
                 break;
                 default:
-                    valor_final += ( ( valor_original * regra_applicada[0].qtd_juros) / 100 ) * diferenca_dias;
-                    valor_final += ( valor_original * regra_applicada[0].qtd_multa ) / 100;
+                    valor_final += ( ( valor_original * regra_aplicada[0].qtd_juros) / 100 ) * diferenca_dias;
+                    valor_final += ( valor_original * regra_aplicada[0].qtd_multa ) / 100;
                     valor_final = valor_final.toFixed(2);
                 break;
             }
         }
 
-        const id_regra = regra_applicada[0] ? regra_applicada[0].id_regra : undefined;
+        const id_regra = regra_aplicada[0] ? regra_aplicada[0].id_regra : undefined;
         const transact = await knex.transaction();
         const insert = await transact('contas').insert({
             nome_conta, 
@@ -49,7 +56,8 @@ export async function createConta (req: Request, res: Response) {
             data_pagamento, 
             diferenca_dias,
             valor_final,
-            id_regra
+            id_regra,
+            tipo_juros
         });
 
         await transact.commit();
@@ -59,6 +67,14 @@ export async function createConta (req: Request, res: Response) {
             id_conta: insert[0]
         });
     } catch (e) {
+        if (e && e.code && e.code === 'ECONNREFUSED'){
+            return res.status(503).json({msg:'Falha na comunicação com o banco de dados. Tente novamente.'});
+        }
+
+        if (e && e.errno) {
+            return res.status(400).json({msg: 'Verifique os dados enviados.', error: e.sqlMessage ? e.sqlMessage : 'desconhecido'});
+        }
+
         return res.status(500).json({msg: 'Erro ao cadastrar conta.', error : e});
     }
 }
