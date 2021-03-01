9919d505d433368a122907d30af93b3871acc064
Tálisson Costa <61150386+talissonc@users.noreply.github.com>
2021-02-28 21:27:41 -0300

    Update index.ts

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -5,14 +5,14 @@ import moment from 'moment';
 export async function showContas (req: Request, res: Response) {
     try {
         const contas = await knex('contas')
-            .select('*')
+            .select('*') // nomes dos campos, indpendentes se vc vai trazer todos, é importante que vc nomeie o que esta vindo..
             .leftJoin('regras_atraso', 'contas.id_regra', 'regras_atraso.id_regra');
 
-        if (contas.length < 1) return res.status(204).json({msg: 'Nenhuma conta encontrada.', contas});
+        if (contas.length < 1) return res.status(204).json({msg: 'Nenhuma conta encontrada.', contas}); //!contas.length 
 
         return res.status(200).json({ msg: 'Contas encontradas com sucesso.', contas });
     } catch(e) {
-        if (e && e.code && e.code === 'ECONNREFUSED'){
+        if (e && e.code && e.code === 'ECONNREFUSED'){ // e?.code == 'ECONNREFUSED' resolve o problema
             return res.status(503).json({msg:'Falha na comunicação com o banco de dados. Tente novamente.'});
         }
 
@@ -23,15 +23,17 @@ export async function showContas (req: Request, res: Response) {
 export async function createConta (req: Request, res: Response) {
     try {
         const { nome_conta, valor_original, data_vencimento, data_pagamento, tipo_juros } = req.body;
-        const regras = await knex('regras_atraso').select('*');
+        const regras = await knex('regras_atraso').select('*'); // nomes dos campos, indpendentes se vc vai trazer todos, é importante que vc nomeie o que esta vindo..
         const diferenca_dias = moment(data_pagamento, 'YYYY-MM-DD').diff(moment(data_vencimento, 'YYYY-MM-DD'), 'days');
+        
+        //Eu criaria uma funçao para buscar as regras aplicadas
         let valor_final = valor_original;
-        const regra_aplicada = regras.filter( regra => {
+        const regra_aplicada = regras.filter( regra => { // Aqui não caberia um find já que o elemento é único ? ou pode ter mais ? 
             if(!regra.qtd_dias_final) regra.qtd_dias_final = Infinity;
-            return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias;
+            return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias; // Ficou confuso isso man, sera que não da pra criar uma função com um nome que explica essas comparações ?
         });
 
-        if(regra_aplicada.length > 0){
+        if(regra_aplicada.length > 0){ // Aqui você não precisaria de um switch case, resolveria um um ternário e tbm não precisaria verificar o length se o find der certo--- mas também criaria outra função com um nome bem descritivo pra ficar claro
             switch (tipo_juros){
                 case 'composto':
                     valor_final = calculaComposto(valor_original, regra_aplicada[0].qtd_juros, regra_aplicada[0].qtd_multa, diferenca_dias);
@@ -42,8 +44,9 @@ export async function createConta (req: Request, res: Response) {
             }
         }
 
-        const id_regra = regra_aplicada[0] ? regra_aplicada[0].id_regra : undefined;
-        const transact = await knex.transaction();
+        // O que acontece se não for encontrada nenhuma regra_aplicada ? vai dar pauu ? se for o caso nem deixa seguir, ja larga um Exception 
+        const id_regra = regra_aplicada[0] ? regra_aplicada[0].id_regra : undefined;  
+        const transact = await knex.transaction(); // Uma função pra inserir não cairia bem será ?
         const insert = await transact('contas').insert({
             nome_conta, 
             valor_original, 
@@ -73,13 +76,13 @@ export async function createConta (req: Request, res: Response) {
         return res.status(500).json({msg: 'Erro ao cadastrar conta.', error : e});
     }
 }
-
+//calcula_____Composto == calculaJuroComposto ?
 export function calculaComposto(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number) {
     let valor_final = valor_original * ( Math.pow( 1 + ( qtd_juros / 100 ) , diferenca_dias ) );
     valor_final += ( valor_original * qtd_multa ) / 100;
     return valor_final.toFixed(2);
 }
-
+//calcula_____Simples == calculaJuroSimples ?
 export function calculaSimples(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number) {
     let valor_final = valor_original + ( ( ( valor_original * qtd_juros) / 100 ) * diferenca_dias);
     valor_final += ( valor_original * qtd_multa ) / 100;
